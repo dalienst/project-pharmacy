2a5f698c9a143c944646ebba19763abaca1294fc
dalienst <oduordalienst@gmail.com>
2022-08-26 08:34:25 +0300

    fix failing deployment

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["projectpharnacy.herokuapp.com"]
+ALLOWED_HOSTS = ['project-pharnacy.herokuapp.com' ]
 
 
 # Application definition
