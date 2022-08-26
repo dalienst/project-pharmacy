305c2adc48bd022b5398ac7042bcc7d2ffd1b0b0
dalienst <oduordalienst@gmail.com>
2022-08-26 09:42:41 +0300

    fix failing deployment

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -159,11 +159,6 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.MultiPartRenderer",
         "rest_framework.renderers.JSONRenderer",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend"
-    ],
 }
 
 SIMPLE_JWT = {
